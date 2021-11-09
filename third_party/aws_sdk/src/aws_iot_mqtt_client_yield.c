0b59cdbb805efd2a51d2f822f1d6aae2aef85416
Chad Elliott <elliottc@objectcomputing.com>
2021-11-09 12:38:54 -0600

    Corrected the conversion of RSI error codes to IoT_Error_t and check for that in yield code.

--- third_party/aws_sdk/src/aws_iot_mqtt_client_yield.c
+++ third_party/aws_sdk/src/aws_iot_mqtt_client_yield.c
@@ -234,7 +234,8 @@ static IoT_Error_t _aws_iot_mqtt_internal_yield(AWS_IoT_Client *pClient, uint32_
 			yieldRc = _aws_iot_mqtt_keep_alive(pClient);
 		} else {
 			// SSL read and write errors are terminal, connection must be closed and retried
-			if(NETWORK_SSL_READ_ERROR == yieldRc || NETWORK_SSL_WRITE_ERROR == yieldRc || NETWORK_SSL_WRITE_TIMEOUT_ERROR == yieldRc) {
+			// RSI error codes will be translated into 'FAILURE'
+			if(FAILURE == yieldRc || NETWORK_SSL_READ_ERROR == yieldRc || NETWORK_SSL_WRITE_ERROR == yieldRc || NETWORK_SSL_WRITE_TIMEOUT_ERROR == yieldRc) {
 				yieldRc = _aws_iot_mqtt_handle_disconnect(pClient);
 			}
 		}
